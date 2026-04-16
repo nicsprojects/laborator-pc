8e240bd1e9ce7ce9d028594da2fc1f425acadbdd
Nicolae-Dan Nechifor <nicudannechifor@gmail.com>
2026-04-16 13:26:35 +0300

    laborator8

--- common/src/SpiMaster.cpp
+++ common/src/SpiMaster.cpp
@@ -1,6 +1,8 @@
 #include <SpiMaster.h>
+#include <avr/io.h>
 
-void SpiMaster::init() {
+void SpiMaster::init()
+{
     // TODO: 1. configure SPI
     /* ---!VERY IMPORTANT!--- */
     // write 1 on CS after set CS as OUTPUT
@@ -12,12 +14,25 @@ void SpiMaster::init() {
     // check ADXL datasheet for clock frequency
     // check ADXL datasheet for clock polarity
     // check ADXL datasheet for clock phase
+
+    DDRB |= (1 << DDB2);
+    PORTB |= (1 << PORTB2);
+    DDRB |= (1 << DDB3) | (1 << DDB5);
+    DDRB &= ~(1 << DDB4);
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << CPOL) | (1 << CPHA);
 }
 
-char SpiMaster::transmit(const char& d) {
+char SpiMaster::transmit(const char &d)
+{
     // TODO: 2. transmit 1 byte on SPI
     // start transmission
     // wait for transmission complete
     // return response from slave
-    return 0x00;
+
+    SPDR = d;
+    while (!(SPSR & (1 << SPIF)))
+    {
+    }
+
+    return SPDR;
 }
--- common/src/Uart.cpp
+++ common/src/Uart.cpp
@@ -1,37 +1,63 @@
 #include <Uart.h>
 #include <string.h>
+#include <stdlib.h>
+#include <avr/io.h>
 
-void Uart::init() {
+#define F_CPU 16000000UL
+
+
+void Uart::init()
+{
     // TODO: 1. configure UART
     // set UART pin directions
     // set baud rate 9600
     // enable RX and TX
     // set frame format: 8 bits, 1 stop bit, no parity
     // DO NOT CHANGE ALL BITS
+    DDRD &= ~(1 << DDD0);
+    DDRD |= (1 << DDD1);
+    int baudrate = 9600;
+    uint16_t ubrr = F_CPU / 16 / baudrate - 1;
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+    UCSR0C &= ~((1 << USBS0) | (1 << UPM01) | (1 << UPM00));
+    UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
 }
 
-void Uart::writeByte(const char& d) {
+void Uart::writeByte(const char &d)
+{
     // TODO: 2. implement write + test it and capture on oscilloscope
     // wait to empty transmit buffer
     // write data
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = d;
 }
 
-bool Uart::available() {
+bool Uart::available()
+{
     // TODO: 3. check if are data in UART buffer
-    return false;
+    return (UCSR0A & (1 << RXC0)) != 0;
 }
 
-char Uart::readByte() {
+char Uart::readByte()
+{
     // TODO: 3. implement read
     // wait for data
     // read data and return it
+    while (!(UCSR0A & (1 << RXC0)));
+    return UDR0;
 }
 
-void Uart::writeString(const char* msg) {
+void Uart::writeString(const char *msg)
+{
     for (int i = 0; i < strlen(msg); i++)
         writeByte(msg[i]);
 }
 
-void Uart::writeIntegerNumber(const int &number, const int &base) {
-    
+void Uart::writeIntegerNumber(const int &number, const int &base)
+{
+    char buffer[17];
+    itoa(number, buffer, base);
+    writeString(buffer);
 }
