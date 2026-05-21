40b5ca924dc5e0011fb665636f1df87e3e291bf2
Nicolae-Dan Nechifor <nicudannechifor@gmail.com>
2026-05-21 13:47:41 +0300

    MERGEEEE

--- OSI/src/main.cpp
+++ OSI/src/main.cpp
@@ -8,57 +8,66 @@
 #include "I2cMaster.h"
 #include "SpiMaster.h"
 
-
-class IAdxlTransport {
+class IAdxlTransport
+{
 public:
     virtual void writeRegister(char reg, char data) = 0;
     virtual char readRegister(char reg) = 0;
 };
 
 // TODO: 1. based on IAdxlTransport interface implement two other classes
-class I2cTransport : public IAdxlTransport {
+class I2cTransport : public IAdxlTransport
+{
     I2cMaster i2c;
     const char addr = 0x53; // ADXL345 I2C address
 public:
-    I2cTransport() {
+    I2cTransport()
+    {
         i2c.init();
     }
-    void writeRegister(char reg, char data) override {
+    void writeRegister(char reg, char data) override
+    {
         i2c.sendStart();
         i2c.writeAddrWrite(addr);
         i2c.writeByte(reg, ACKNOWLEDGE);
         i2c.writeByte(data, ACKNOWLEDGE);
         i2c.sendStop();
     }
-    char readRegister(char reg) override {
+    char readRegister(char reg) override
+    {
         char data;
         i2c.sendStart();
         i2c.writeAddrWrite(addr);
-        i2c.writeByte(reg, ACKNOWLEDGE );
+        i2c.writeByte(reg, ACKNOWLEDGE);
         i2c.sendStart();
         i2c.writeAddrRead(addr);
         i2c.readByte(data, NOT_ACKNOWLEDGE);
         i2c.sendStop();
         return data;
     }
-}; 
+};
 
 // - one SpiTransport for SPI protocol
-class SpiTransport : public IAdxlTransport {
+class SpiTransport : public IAdxlTransport
+{
     SpiMaster spi;
+
 public:
-    SpiTransport() {
+    SpiTransport()
+    {
         spi.init();
     }
 
-    void writeRegister(char reg, char data) override {
+    void writeRegister(char reg, char data) override
+    {
         PORTB &= ~(1 << PORTB2); // CS low
         spi.transmit(reg);
         spi.transmit(data);
         PORTB |= (1 << PORTB2); // CS high
     }
-    char readRegister(char reg) override {
-        PORTB &= ~(1 << PORTB2); // CS low
+    char readRegister(char reg) override
+    {
+        PORTB &= ~(1 << PORTB2);  // CS low
         spi.transmit(reg | 0x80); // set read bit
         char data = spi.transmit(0xFF);
         PORTB |= (1 << PORTB2); // CS high
@@ -67,51 +76,63 @@ public:
 };
 
 // TODO: 2. implement Adxl high level class
-class Adxl {
-    IAdxlTransport* transport; 
+class Adxl
+{
+    IAdxlTransport *transport;
     int16_t x, y, z;
     char lastReadAxis;
+
 public:
-    Adxl(IAdxlTransport * t) : transport(t), x(0), y(0), z(0), lastReadAxis(0) {}
-    
-    bool isConnected() {
-        return transport->readRegister(0x00) == 0xE5; // device id register
+    Adxl(IAdxlTransport *t) : transport(t), x(0), y(0), z(0), lastReadAxis(0) {}
+
+    bool isConnected()
+    {
+        return (unsigned char)transport->readRegister(0x00) == 0xE5;
     }
-    void startMeasurement() {
-        transport->writeRegister(0x2D, 0x08); 
+    void startMeasurement()
+    {
+        transport->writeRegister(0x2D, 0x08);
     }
-    void stopMeasurement() {
-        transport->writeRegister(0x2D, 0x00); 
+    void stopMeasurement()
+    {
+        transport->writeRegister(0x2D, 0x00);
     }
-    void readData(){
+    void readData()
+    {
         char x0 = transport->readRegister(0x32); // data register for x-axis
         char x1 = transport->readRegister(0x33); // data register for x-axis
         char y0 = transport->readRegister(0x34); // data register for y-axis
         char y1 = transport->readRegister(0x35); // data register for y-axis
         char z0 = transport->readRegister(0x36); // data register for z-axis
         char z1 = transport->readRegister(0x37); // data register for z-axis
-        
+
         x = (int16_t)((x1 << 8) | (x0 & 0XFF));
         y = (int16_t)((y1 << 8) | (y0 & 0XFF));
         z = (int16_t)((z1 << 8) | (z0 & 0XFF));
     }
-    
-    int16_t getX() {
-        if (lastReadAxis != 'X') {
+
+    int16_t getX()
+    {
+        if (lastReadAxis != 'X')
+        {
             readData();
             lastReadAxis = 'X';
         }
         return x;
     }
-    int16_t getY() {
-        if (lastReadAxis != 'Y') {
+    int16_t getY()
+    {
+        if (lastReadAxis != 'Y')
+        {
             readData();
             lastReadAxis = 'Y';
         }
         return y;
     }
-    int16_t getZ() {
-        if (lastReadAxis != 'Z') {
+    int16_t getZ()
+    {
+        if (lastReadAxis != 'Z')
+        {
             readData();
             lastReadAxis = 'Z';
         }
@@ -120,34 +141,39 @@ public:
 };
 
 Uart uart;
-Adxl* adxl;
+Adxl *adxl;
 
 // TODO: 3. TEST IT!
-void setup() {
+void setup()
+{
     // initialize GDB stub
     uart.init();
-    
-    adxl = new Adxl(new SpiTransport());
-    
-    if (adxl->isConnected()) {
-        uart.writeString("s-o conectat bAAAAAAAAAAA");
+
+    adxl = new Adxl(new I2cTransport());
+
+    if (adxl->isConnected())
+    {
+        uart.writeString("s-a conectat");
         adxl->startMeasurement();
     }
-    else {
-        uart.writeString("esti vai de mortii tai, nu s-a conectat");
+    else
+    {
+        uart.writeString("nu e boss");
     }
 }
 
-void loop() {
-    if (adxl->isConnected()) {
-       adxl->readData();
-       uart.writeString("X: ");
-       uart.writeIntegerNumber(adxl->getX(), 10);
-       uart.writeString("Y: ");
-       uart.writeIntegerNumber(adxl->getY(), 10);
-       uart.writeString("Z: ");
-       uart.writeIntegerNumber(adxl->getZ(), 10);
-       uart.writeString("\r\n");
-       delay(500);
+void loop()
+{
+    if (adxl->isConnected())
+    {
+        adxl->readData();
+        uart.writeString("X: ");
+        uart.writeIntegerNumber(adxl->getX(), 10);
+        uart.writeString("Y: ");
+        uart.writeIntegerNumber(adxl->getY(), 10);
+        uart.writeString("Z: ");
+        uart.writeIntegerNumber(adxl->getZ(), 10);
+        uart.writeString("\r\n");
+        delay(500);
     }
 }
--- common/src/I2cMaster.cpp
+++ common/src/I2cMaster.cpp
@@ -1,45 +1,55 @@
 #include <I2cMaster.h>
 #include <avr/io.h>
 
-void I2cMaster::_writeByte(const char& data) {
+void I2cMaster::_writeByte(const char &data)
+{
     // TODO: 2. send 1 byte on I2C
     // write byte
     // send command to send byte
     // wait for ack or nack
     TWDR = data;
     TWCR = (1 << TWINT) | (1 << TWEN);
-    while (!(TWCR & (1 << TWINT)));
-}
+    while (!(TWCR & (1 << TWINT)))
+        ;
+}  
 
-void I2cMaster::_readByte(char& data) {
+void I2cMaster::_readByte(char &data)
+{
     // TODO: 2. read 1 byte from I2C
     // send command to read byte
     // wait for ack or nack
     // read byte
-    
+
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    while (!(TWCR & (1 << TWINT)))
+        ;
+    data = TWDR;
 }
 
-void I2cMaster::init() {
+void I2cMaster::init()
+{
     // TODO: 1. configure SPI for master mode
     // set frequency; read datasheet for SCLK frequency
     // set SCLK pin output
     // enable internal pull up resistor for SCLK and SDA, set PORTxn = 1
-    TWSR = 0x00; // prescaler = 1
-    TWBR = 72; // SCL frequency = F_CPU / (16 + 2 * TWBR * prescaler) = 100kHz
-    TWCR = (1 << TWEN); // enable TWI
+    TWSR = 0x00;                            // prescaler = 1
+    TWBR = 72;                              // SCL frequency = F_CPU / (16 + 2 * TWBR * prescaler) = 100kHz
+    TWCR = (1 << TWEN);                     // enable TWI
     PORTC |= (1 << PORTC4) | (1 << PORTC5); // enable pull-up resistors for SCL and SDA
 }
 
-void I2cMaster::sendStart() {
+void I2cMaster::sendStart()
+{
     // TODO: 2. send I2C start condition
     // send start condition
     // wait for start has been transmitted
     TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
-    while (!(TWCR & (1 << TWINT)));
-
+    while (!(TWCR & (1 << TWINT)))
+        ;
 }
 
-bool I2cMaster::writeAddrWrite(const char& addr) {
+bool I2cMaster::writeAddrWrite(const char &addr)
+{
     // TODO: 2. send slave addr for write
     // add write bit to addr {addr[6:0],r/w}
     _writeByte((addr << 1) | 0);
@@ -47,7 +57,8 @@ bool I2cMaster::writeAddrWrite(const char& addr) {
     return (TWSR & 0xF8) == 0x18; // SLA+W transmitted and ACK received
 }
 
-bool I2cMaster::writeAddrRead(const char& addr) {
+bool I2cMaster::writeAddrRead(const char &addr)
+{
     // TODO: 2. send slave addr for read
     // add read bit to addr {addr[6:0],r/w}
     _writeByte((addr << 1) | 1);
@@ -55,7 +66,8 @@ bool I2cMaster::writeAddrRead(const char& addr) {
     return (TWSR & 0xF8) == 0x40; // SLA+R transmitted and ACK received
 }
 
-bool I2cMaster::writeByte(const char& d, I2cResponse expectedResponse) {
+bool I2cMaster::writeByte(const char &d, I2cResponse expectedResponse)
+{
     // TODO: 2. send byte on I2C
     // send byte
     _writeByte(d);
@@ -63,20 +75,24 @@ bool I2cMaster::writeByte(const char& d, I2cResponse expectedResponse) {
     return (TWSR & 0xF8) == 0x28; // Data transmitted and ACK received
 }
 
-bool I2cMaster::readByte(char& d, I2cResponse expectedResponse) {
-    // TODO: 2. read byte from I2C
-    if (expectedResponse){
+bool I2cMaster::readByte(char &d, I2cResponse expectedResponse)
+{
+    if (expectedResponse == ACKNOWLEDGE)
+    {
         TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA); // Enable ACK after reception
-    } else {
-        TWCR = (1 << TWINT) | (1 << TWEN); // No ACK after reception
     }
-    while (!(TWCR & (1 << TWINT))); // Wait for reception to complete
-    d = TWDR; // Read received byte
-    // retrun if ack received
+    else
+    {
+        TWCR = (1 << TWINT) | (1 << TWEN); // No ACK after reception (trimite NACK)
+    }
+    while (!(TWCR & (1 << TWINT)))
+        ;     
+    d = TWDR; 
     return true;
 }
 
-void I2cMaster::sendStop() {
+void I2cMaster::sendStop()
+{
     // TODO: 2. send I2C stop condition
     TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
 }
